82e5ca1cdb15bd2ec7f97991a748d11bb9408c46
Chinmay-G <chinmay.the1@gmail.com>
2024-09-05 16:21:51 +0530

    Done 4 exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let arr = string.split("");
+    // let arrnew = [];
+    // for (let i = arr.length - 1, j = 0; i >= 0; i--, j++) {
+    //     arrnew[j] = arr[i];
+    // }
 
+    let str = '';
+    for (let i = arr.length - 1; i >= 0; i--) {
+        str += arr[i];
+    }
+    return str;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...r) {
+    for (let k = 0; k < arguments.length - 1; k++) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === r[k]) {
+                for (let j = i; j < arr.length - 1; j++) {
+                    arr[j] = arr[j + 1];
+                }
+                i--;
+                arr.pop();
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
