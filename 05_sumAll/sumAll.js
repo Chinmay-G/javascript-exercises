e66833d74298ef0d190ebfb4badceb51dd57a623
Chinmay-G <chinmay.the1@gmail.com>
2024-09-06 10:17:24 +0530

    Solved exercises 5, 6, and 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,35 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+    let isIntx = Number.isInteger(x);
+    let isInty = Number.isInteger(y);
+    let isPositivex = Math.sign(x);
+    let isPositivey = Math.sign(y);
+    if (!isIntx || !isInty || isPositivex !== 1 || isPositivey !== 1) {
+        return 'ERROR';
+    }
 
+    if (x > y) {
+        let temp = x;
+        x = y;
+        y = temp;
+    }
+
+    let sum = 0;
+    for (let i = x; i <= y; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+/*
+1) Understanding the problem
+- 2nd parameter can be smaller than the 1st
+- If the parameter is not a positive integer, return ERROR
+
+2) Breaking down the problem
+- How to verify if the parameter is a positive integer?
+- How to go through the numbers in between, including the parameters?
+*/
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    if (year % 400 === 0) return true;
+    else if (year % 100 === 0) return false;
+    else if (year % 4 === 0) return true;
+    else return false;
 };
+
+/*
+1) Understanding the problem
+- leap years are divisible by 4, but not by 100, unless it's divisible by 400.
+
+2) Breaking down the problem
+- First check if it's diisible by 400, then 100, then 4 and return in each step if true
+*/
 
 // Do not edit below this line
 module.exports = leapYears;
